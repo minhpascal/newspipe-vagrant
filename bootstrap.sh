9215fa9bf473827498d61cdd2f32465303a1329c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-04-09 00:01:49 +0200

    Fixed some problems with Vagrant bootstrap procedure.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y python3 python3-dev libpq-dev python3-pip build-essential git
+apt-get install -y python3 python3.5-dev libpq-dev python3-pip build-essential git
 
 # Clone the source code of JARR
 git clone https://github.com/JARR-aggregator/JARR.git
@@ -26,9 +26,14 @@ apt-get install -y libxml2-dev libxslt1-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3.5 get-pip.py
 rm get-pip.py
-sudo pip3 install --upgrade -r requirements.txt
-# copy of the default configuration files for vagrant
-cp vagrant/conf.cfg-sample src/conf/conf.cfg
+sudo pip3.5 install --upgrade -r requirements.txt
+
+# Set the default configuration files for vagrant
+cp src/conf/conf.cfg-sample src/conf/conf.cfg
+sed -i '/database/d' src/conf/conf.cfg
+sed -i '/database_url/d' src/conf/conf.cfg
+echo '[database]' >> src/conf/conf.cfg
+echo 'database_url = postgres://vagrant:xxYzToW42@127.0.0.1:5432/aggregator' >> src/conf/conf.cfg
 cd ..
 
 # Configuration of the database
@@ -42,24 +47,25 @@ echo "GRANT ALL PRIVILEGES ON DATABASE aggregator TO vagrant;" | sudo -u postgre
 # Initializes the database
 cd JARR
 chown -R vagrant:vagrant .
-sudo -u vagrant python3 src/manager.py db_empty
-sudo -u vagrant python3 src/manager.py db_create
+sudo -u vagrant python3.5 src/manager.py db_empty
+sudo -u vagrant python3.5 src/manager.py db_create
 
-# start JARR at startup
+# Start JARR at startup
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "cd /home/vagrant/JARR/" >> /etc/rc.local
-echo "sudo -u vagrant python3 src/runserver.py &" >> /etc/rc.local
+echo "sudo -u vagrant python3.5 src/runserver.py &" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 chmod 755 /etc/rc.local
 
 # Start the application.
-sudo /etc/init.d/rc.local start
+#sudo /etc/init.d/rc.local start
+sudo -u vagrant nohup python3.5 src/runserver.py &
 
 
 #write out current crontab
 sudo -u vagrant crontab -l > mycron
 #echo new cron into cron file
-sudo -u vagrant echo "*/30 * * * * cd /home/vagrant/JARR/ ; python3 src/manager.py fetch_asyncio None None" >> mycron
+sudo -u vagrant echo "*/30 * * * * cd /home/vagrant/JARR/ ; python3.5 src/manager.py fetch_asyncio None None" >> mycron
 #install new cron file
 sudo -u vagrant crontab mycron
 sudo -u vagrant rm mycron
