2f10023605f40fe9eb1aa0d105ff0dfc0f36252a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-09-27 15:13:05 +0200

    removed useless environment variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-export DEBIAN_FRONTEND=noninteractive
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-sudo locale-gen en_US.UTF-8
-sudo dpkg-reconfigure locales
 
 sudo apt-get update
 sudo apt-get upgrade
